fe8f49b0731eb459ef2f94c61679f585ff828523
Quentin Vanderlinden <vdlquentin@hotmail.comvdlquentin@hotmail.comgit config --global user.name QuentinQuentingit commit -m Premier>
2018-02-06 12:14:02 +0100

    js

---
+++ app/app.js
@@ -0,0 +1,38 @@
+// Récupérer le bouton
+let bouton = document.querySelector('#add');
+// Récupérer les email
+let imput = document.querySelector('# email');
+// click sur le bouton pour envoyer le message  
+let button1 = function () {
+    if (bouton = "submit") {
+        console.log(bouton);
+        let list = document.createElement('li');
+        list.innerHTML = monInput;
+    }
+
+}
+let newInput = function () {
+    input.value = " ";
+}
+
+
+// mettre les écouteurs d'événement
+
+let bouton = addEventListener("click")
+
+$('.carousel').carousel({
+    interval: 2000
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
